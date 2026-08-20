80598403e089f2d09af5c36fb5d1a32ff6b737e6
Stefan2320 <stefan.paraschiv023@gmail.com>
2026-08-20 15:42:03 +0300

    secret and sast test

---
+++ config/secrets7.js
@@ -0,0 +1,8 @@
+// Dummy secrets for local PR autotriage testing (not real credentials)
+module.exports = {
+  api_key: "aik_live_9f3c2e7b1a4d8e6c0b5f2a7d9e1c4b8f3a6d0e2c5b7a9f1d3e6c8b0a2",
+  client_secret:
+    "cs_live_h8K2mP9qR4vN7bX1zT5wY3jL6cF0aD8eG2sU4iO9pA1mK7nB5vC3xZ",
+  auth_token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+};
